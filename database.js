748237710a84426db9ce6425841c2317a0925bac
Pablo Goeppinger <pgoeppin@gmail.com>
2022-12-30 06:27:38 -0300

    Using public folder and adding validations

--- database.js
+++ database.js
@@ -1,38 +1,33 @@
-require('dotenv').config()
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    allowExitOnIdle: true, 
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  allowExitOnIdle: true,
 });
 
 const createPost = async (payload) => {
-    const query = {
-        text: "INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING *",
-        values: [
-            payload.titulo,
-            payload.img,
-            payload.descripcion,
-            payload.likes
-        ]
-    }
-    try {
-        const result = await pool.query(query)
-        console.log("Post creado")
-        return result.rows
-    } catch(e) {
-        console.log('Error al insertar datos en tabla posts:', e.code, e.message)
-        throw new Error(e)
-    }
-}
+  const query = {
+    text: "INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING *",
+    values: [payload.titulo, payload.url, payload.descripcion, 0],
+  };
+  try {
+    const result = await pool.query(query);
+    console.log("Post creado");
+    return result.rows;
+  } catch (e) {
+    console.log("Error al insertar datos en tabla posts:", e.code, e.message);
+    throw new Error(e);
+  }
+};
 
 const getPosts = async () => {
-    const { rows } = await pool.query("SELECT * FROM posts")
-    console.log(rows)
-    return rows
-}
+  const { rows } = await pool.query("SELECT * FROM posts");
+  console.log(rows);
+  return rows;
+};
 
 module.exports = { createPost, getPosts };
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const { createPost, getPosts } = require("./database")
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
+const { createPost, getPosts } = require("./database");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-
-app.listen(process.env.PORT, console.log("SERVIDOR ENCENDIDO"))
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+app.listen(process.env.PORT, console.log("SERVIDOR ENCENDIDO"));
 
 app.get("/posts", async (req, res) => {
-    const posts = await getPosts()
-    res.json(posts)
-})
+  const posts = await getPosts();
+  res.json(posts);
+});
 
 app.post("/posts", async (req, res) => {
-    const payload = req.body
-    await createPost(payload)
-    res.send("Post agregado con exito!")
-})
+  const payload = req.body;
+  if (
+    !payload.titulo ||
+    !payload.url ||
+    !payload.descripcion ||
+    (payload.url.endsWith(".jpg") === false &&
+      payload.url.endsWith(".png") === false)
+  ) {
+    return res.status(400).json({
+      message:
+        "Error 400. Por favor, rellena todos los campos. Y la URL debe terminar en .jpg o .png",
+    });
+  }
+  await createPost(payload);
+  res.send("Post agregado con exito!");
+});
