4994b800a11d93df8a46176c53d0952b252d8596
Pablo Goeppinger <pgoeppin@gmail.com>
2022-12-30 04:09:58 -0300

    Adding GET and POST features

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const { createPost, getPosts } = require("./database")
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const test = {
-    titulo: "Hola",
-    img: "no im",
-    descripcion: "este es un test",
-    likes: 2
-}
-// createPost(test);
-getPosts();
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, console.log("SERVIDOR ENCENDIDO"))
+
+app.get("/posts", async (req, res) => {
+    const posts = await getPosts()
+    res.json(posts)
+})
+
+app.post("/posts", async (req, res) => {
+    const payload = req.body
+    await createPost(payload)
+    res.send("Post agregado con exito!")
+})
